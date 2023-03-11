15db2f3508fea2f739d9be085eb7a91489b598d2
Gustavo Lara <gustavo.silvalara@outlook.com>
2023-03-11 12:48:56 -0400

    add capacitor local notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
 
+import { LocalNotifications } from '@capacitor/local-notifications';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.setNotification();
+  }
+
+  private setNotification(): void {
+    LocalNotifications.requestPermissions();
+
+    const now = new Date();
+    const tomorrow = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate() + 1
+    );
+    const midnightUTC = new Date(
+      Date.UTC(
+        tomorrow.getFullYear(),
+        tomorrow.getMonth(),
+        tomorrow.getDate(),
+        0,
+        0,
+        0
+      )
+    );
+
+    LocalNotifications.schedule({
+      notifications: [
+        {
+          title: 'Novos Itens na Loja!',
+          body: 'A loja foi atualizada, entre para conferir o que há de novo!',
+          id: 1,
+          schedule: {
+            every: 'day',
+            at: midnightUTC,
+          },
+        },
+      ],
+    });
+  }
 }

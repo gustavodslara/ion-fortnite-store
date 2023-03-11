06adc8308f5d8c7bcf81e6666146582e240e4777
Gustavo Lara <gustavo.silvalara@outlook.com>
2023-03-11 16:29:04 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  fortniteApiUrl: 'https://fortniteapi.io/v2/shop?lang=', //pt-BR
-  fortniteApiKey: '',
+  production: false,
+  fortniteApiUrl: 'https://fortniteapi.io/v2/shop?lang=',//pt-BR
+  fortniteApiKey: process.env["FORTNITE_API_KEY"],
   googleApiUrl: 'https://translation.googleapis.com/language/translate/v2?key=',
-  googleApiKey: '',
+  googleApiKey: process.env["GOOGLE_API_KEY"],
 };
 
 /*

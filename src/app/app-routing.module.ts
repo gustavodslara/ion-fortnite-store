7d4aec09b7e2b67a4c712819d341058220087404
Gustavo Lara <gustavo.silvalara@outlook.com>
2023-03-19 19:29:41 -0400

    fix dont loading page on open

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomePageModule),
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  fortniteApiUrl: 'https://fortniteapi.io/v2/shop?lang=',//pt-BR
-  fortniteApiKey: process.env["FORTNITE_API_KEY"],
+  fortniteApiUrl: 'https://fortniteapi.io/v2/shop?lang=', //pt-BR
+  fortniteApiKey: process.env['FORTNITE_API_KEY'],
   googleApiUrl: 'https://translation.googleapis.com/language/translate/v2?key=',
-  googleApiKey: process.env["GOOGLE_API_KEY"],
+  googleApiKey: process.env['GOOGLE_API_KEY'],
 };
 
 /*
